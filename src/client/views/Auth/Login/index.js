3d7f34d7c8a0baed8fe448c6c28de7c5c3751681
Jaspreet Singh <jaspreet_xps@yahoo.com>
2017-05-31 21:03:21 -0700

    Add font awesome, make login screen

--- src/client/views/Auth/Login/index.js
+++ src/client/views/Auth/Login/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import FontAwesome from 'react-fontawesome'
+import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField'
 import Paper from 'material-ui/Paper'
 import styles from './styles'
@@ -14,10 +16,19 @@ class Login extends Component {
   render() {
     return (
       <div style={styles.container}>
-        <Paper zDepth={2}>
-          <div style={styles.textFields}>
-            <TextField id="username"/>
-            <TextField id="pw"/>
+        <Paper style={styles.paperContainer} zDepth={2}>
+          <div style={styles.content}>
+            <div style={styles.textFields}>
+              <div style={{ display: "flex", alignItems: "center" }}>
+                <FontAwesome name="user" size="2x" style={{ width: 32 }}/>
+                <TextField style={styles.textField} id="username"/>
+              </div>
+              <div style={{ display: "flex", alignItems: "center" }}>
+                <FontAwesome name="key" size="2x"/>
+                <TextField style={styles.textField} type="password" id="pw"/>
+              </div>
+            </div>
+            <RaisedButton style={styles.signInBtn} label="Sign In" primary/>
           </div>
         </Paper>
       </div>
--- src/client/views/Auth/Login/styles.js
+++ src/client/views/Auth/Login/styles.js
@@ -2,8 +2,27 @@ export default {
   container: {
     width: 300
   },
+  paperContainer: {
+    paddingTop: 8,
+    paddingBottom: 8,
+  },
   textFields: {
     display: "flex",
+    flexDirection: "column",
+    marginLeft: 26,
+    marginRight: 26,
+  },
+  content: {
+    display: "flex",
     flexDirection: "column"
+  },
+  signInBtn: {
+    alignSelf: "flex-end",
+    marginRight: 26,
+    marginTop: 8
+  },
+  textField: {
+    marginLeft: 8,
+    width: 240
   }
 }
