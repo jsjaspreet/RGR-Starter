193cded369f275cc7868ef7813135db9a94bf282
Jaspreet Singh <jaspreet_xps@yahoo.com>
2017-06-05 21:36:19 -0700

    Refactor for router ease

---
+++ src/client/views/Auth/container.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { QueryRenderer, graphql } from 'react-relay'
+import Auth from './index'
+import environment from '../../util/relayEnvironment'
+
+export default () => (
+  <QueryRenderer
+    environment={environment}
+    query={graphql`
+           query containerQuery {
+             root {
+               ...Auth_root
+             }
+           }
+         `}
+    render={({ error, props }) => {
+      if (error) {
+        return <div>{error.message}</div>
+      } else if (props) {
+        return <Auth {...props} />
+      }
+      return <div>Loading</div>
+    }}
+  />
+)
--- src/client/views/Root/index.js
+++ src/client/views/Root/index.js
@@ -1,31 +1,12 @@
 import React, { Component } from 'react'
 import styles from './styles'
-import Auth from '../Auth'
-import environment from '../../util/relayEnvironment'
-import { QueryRenderer, graphql } from 'react-relay'
+import Auth from '../Auth/container'
 
 class App extends Component {
   render() {
     return (
       <div style={styles.container}>
-        <QueryRenderer
-          environment={environment}
-          query={graphql`
-           query RootQuery {
-             root {
-               ...Auth_root
-             }
-           }
-         `}
-          render={({ error, props }) => {
-            if (error) {
-              return <div>{error.message}</div>
-            } else if (props) {
-              return <Auth {...props} />
-            }
-            return <div>Loading</div>
-          }}
-        />
+        <Auth/>
       </div>
     )
   }
