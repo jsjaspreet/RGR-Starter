0e99be5327706a3aa14a31dbdd1fe35ad34f57dd
Jaspreet Singh <jaspreet_xps@yahoo.com>
2017-06-06 23:04:27 -0700

    Integrate routes

--- src/client/views/Root/index.js
+++ src/client/views/Root/index.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom'
 import styles from './styles'
 import Auth from '../Auth/container'
 
 class App extends Component {
   render() {
+    const currPath = window.location.pathname
+    if (currPath === "/") {
+      return <Redirect to="/login"/>
+    }
     return (
       <div style={styles.container}>
-        <Auth/>
+        <Switch>
+          <Route path="/login" component={Auth}/>
+        </Switch>
       </div>
     )
   }
