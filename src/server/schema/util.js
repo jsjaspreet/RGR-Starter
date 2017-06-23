5c223dc8965ebab50178f76d1b0c18883ee8438a
Jaspreet Singh <jaspreet_xps@yahoo.com>
2017-06-23 01:08:09 -0700

    Add dialect explicitly

--- src/server/schema/util.js
+++ src/server/schema/util.js
@@ -3,5 +3,5 @@ import joinMonster from 'join-monster'
 export const joinMonsterResolver = (obj, args, { pgPool }, resolveInfo) => {
   return joinMonster(resolveInfo, {}, sql => {
     return pgPool.query(sql)
-  })
+  }, { dialect: 'pg' })
 }
