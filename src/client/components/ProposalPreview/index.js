eec3efd843fe4c6bc0ff3bdb54f3f7f490bd9b11
Jaspreet Singh <jaspreet_xps@yahoo.com>
2017-07-05 22:49:20 -0700

    Include createdAt and createdBy in the preview component

--- src/client/components/ProposalPreview/index.js
+++ src/client/components/ProposalPreview/index.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import moment from 'moment'
 import { createFragmentContainer, graphql } from 'react-relay'
-import { Card, CardTitle, CardActions, CardText } from 'material-ui/Card'
+import { Card, CardTitle, CardActions } from 'material-ui/Card'
 import RaisedButton from 'material-ui/RaisedButton'
 import styles from './styles'
 
 class ProposalPreview extends Component {
   render() {
     const { proposal } = this.props
-    const { proposalText, proposalSlug } = proposal
+    const { proposalText, proposalSlug, createdAt, createdBy } = proposal
+    const { username } = createdBy
+    const momentCreated = moment(createdAt).format("MMM D YYYY")
     return (
       <Card style={styles.proposalContainer}>
-        <CardTitle title="Proposal"/>
-        <CardText>
-          {proposalText}
-        </CardText>
+        <CardTitle title={proposalText} subtitle={`Created ${momentCreated} by ${username}`}
+                   subtitleStyle={{ fontSize: 12 }}/>
         <CardActions>
           <Link to={`/proposal/${proposalSlug}`}>
             <RaisedButton primary label="See Vote"/>
@@ -32,6 +33,9 @@ export default createFragmentContainer(
           proposalText
           proposalSlug
           createdAt
+          createdBy {
+              username
+          }
       }
   `
 )
