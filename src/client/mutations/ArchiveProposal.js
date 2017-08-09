f25cf4113121a2908ec3836dc9c8df7b638b239a
Jaspreet Singh <jaspreet_xps@yahoo.com>
2017-08-09 00:18:42 -0700

    Add note for future archive proposal mutation

---
+++ src/client/mutations/ArchiveProposal.js
@@ -0,0 +1,3 @@
+// https://github.com/facebook/relay/blob/98ec1d5b3de46f94dc601a26caeab674b8a17a4e/packages/relay-runtime/store/RelayStoreTypes.js#L156
+// Use the setValue in the Updater to toggle active to false
+
